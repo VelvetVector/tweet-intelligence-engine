c76235184850b0d891b231a23ffd40f47629ffc3
VelvetVector <tejasc892@gmail.com>
2026-04-01 19:27:11 +0530

    app_flask.py

--- app_flask.py
+++ app_flask.py
@@ -4,16 +4,12 @@ import numpy as np
 
 app = Flask(__name__)
 
-# ===============================
-# 🔹 Load model + encoders
-# ===============================
+#load modle
 model = joblib.load('like_predictor.pkl')
 company_encoder = joblib.load('company_encoder.pkl')
 global_avg = joblib.load('global_avg.pkl')
 
-# ===============================
-# 🔹 Routes
-# ===============================
+#api
 @app.route('/')
 def home():
     return "API is working 🚀"
@@ -22,14 +18,12 @@ def home():
 def predict():
     try:
         data = request.get_json()
-
-        # 🔹 Extract company
+    
         company = data['company']
 
-        # 🔹 Encode company
         company_target_enc = company_encoder.get(company, global_avg)
 
-        # 🔹 Build feature array (ORDER MATTERS)
+        #features
         features = np.array([
             data['word_count'],
             data['char_count'],
@@ -45,11 +39,11 @@ def predict():
             company_target_enc
         ]).reshape(1, -1)
 
-        # 🔹 Predict
+        #prediction
         prediction = model.predict(features)[0]
 
         return jsonify({
-            'predicted_likes': int(np.expm1(prediction))  # convert back from log
+            'predicted_likes': int(np.expm1(prediction))
         })
 
     except Exception as e:
@@ -57,8 +51,5 @@ def predict():
             'error': str(e)
         }), 500
 
-# ===============================
-# 🔹 Run app
-# ===============================
 if __name__ == '__main__':
     app.run(debug=True)
